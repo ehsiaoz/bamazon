fd2afe557a5d4be8bd12bc9630d601b4c1586c97
en <ehsiaoz@h2cap.com>
2017-01-13 16:04:35 -0600

    challenge 1 complete

--- bamazon.sql
+++ bamazon.sql
@@ -1,15 +1,27 @@
-CREATE DATABASE Bamazon,
-USE Bamazon
+CREATE DATABASE Bamazon;
+USE Bamazon;
 
 CREATE TABLE products (
-item_id VARCHAR(10) NOT NULL AUTO_INCREMENT,
+item_id INT(10) NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(250) NULL,
 department_name VARCHAR(250) NULL,
 price DECIMAL(10,2) NULL,
 stock_quantity INT NULL,
 PRIMARY KEY (item_id)
 );
-    
-SELECT * FROM products
+  
+INSERT INTO products 
+(product_name, department_name, price, stock_quantity)
+VALUES 
+("Nintendo Wii", "Electronics", 100.88, 5),
+("Xbox", "Electronics", 300.54, 10),
+("Basketball", "Sporting Goods", 30, 23),
+("Hockey Stick", "Sporting Goods", 25, 17),
+("Samsung Galaxy S7", "Cell Phones & Accessories", 300.27, 50),
+("iPhone 7", "Cell Phones & Accessories", 510.33, 3),
+("iPhone 7 Case", "Cell Phones & Accessories", 34.99, 20),
+("Air Jorden Retro", "Men's Shoes", 150, 12),
+("Nerf N-Strike Blaster", "Toys & Games", 19.95, 8),
+("Monopoly", "Toys & Games", 25.88, 1);
 
-INSERT INTO products (
+SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,43 +21,65 @@ var displayItems = function(){
 	var query = "SELECT item_id, product_name, price FROM products";
   	connection.query(query, function(err, res) {
 	  	if (err) throw err;
-	    
+
 	    for (var i = 0; i < res.length; i++) {
-	      console.log("Id: " + res[i].item_id + "  || Product Name: " + res[i].product_name + 
+	      console.log("Id: " + res[i].item_id + "  || Product Name: " + res[i].product_name +
 	      			" || Price: " + res[i].price);
 	  	};
 	  	console.log(" ");
 	  	// connection.end();
-	  	purchase();
+	  	purchase(res.length);
 	})
 
-var purchase = function(){
-		inquirer.prompt([
-			{
-			    name: "productID",
-			    type: "input",
-			    message: "Enter the ID of the item you would like to purchase: ",
-			    validate: function(value) {
-				      if (isNaN(value) === false) {
+	var purchase = function(itemIDCount){
+			inquirer.prompt([
+				{
+				    name: "itemID",
+				    type: "input",
+				    message: "Enter the ID of the item you would like to purchase: ",
+				    validate: function(value) {
+					      if (isNaN(value) === false && value > 0 && value <= itemIDCount) {
+					        return true;
+					      }
+					      return false;
+				    	}
+				 },
+			 	 {
+				    name: "quantity",
+				    type: "input",
+				    message: "How many units would you like to purchase? ",
+				    validate: function(value) {
+				      if (isNaN(value) === false && value > 0) {
 				        return true;
 				      }
 				      return false;
-			    	}
-			 },
-		 	 {
-			    name: "quantity",
-			    type: "input",
-			    message: "How many units would you like to purchase? ",
-			    validate: function(value) {
-			      if (isNaN(value) === false) {
-			        return true;
-			      }
-			      return false;
-			    }
-		  },
-		  ]).then(function(answer) {
-		  		console.log(answer);
-		  });
+				    }
+			 	 },
+			  ]).then(function(answer) {
+			  		var query = "SELECT item_id, product_name, stock_quantity, price FROM products WHERE ?";
+				  	connection.query(query, { item_id: answer.itemID }, function(err, res) {
+					  	if (err) throw err;
+					  		if (answer.quantity > res[0].stock_quantity) {
+					  			console.log("Insufficient Quantity!");
+					  		} 
+					  		else {
+					  			var newQuantity = res[0].stock_quantity - answer.quantity;
+					  			var itemID = answer.itemID;
+					  			var productName = res[0].product_name;
+					  			var price = res[0].price;
+					  			var amount = res[0].price * answer.quantity;
+					  			var query = "UPDATE products SET ? WHERE ?";
+					  			connection.query(query, [{stock_quantity: newQuantity}, {item_id: itemID}], function(err, res) {
+					  				console.log("\n" + "Purchase Summary" + "\n" +
+					  					        "================" + "\n" +
+					  					        "Item: " + productName + "\n" +
+					  					        "Price: " + price + "\n" +
+					  					        "Total Cost of Purchase: $" + amount);
+					  			});
+					  		connection.end();
+					  		}
+					});
+				});
+		 
 	};
 }
-
